e0a3b80926a7431496b2aa7042e726ee92e1cab0
ym <225009064+ym-410@users.noreply.github.com>
2026-04-14 06:17:46 +0900

    キャッシュを更新

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'uni-schedule-v1'
-const ASSETS_TO_CACHE = ['/', '/index.html', '/manifest.webmanifest']
+const CACHE_NAME = 'uni-schedule-v2'
+const PRECACHE_URLS = ['/', '/index.html', '/manifest.webmanifest']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE)
+      return cache.addAll(PRECACHE_URLS)
     }),
   )
   self.skipWaiting()
@@ -19,6 +19,25 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') {
+    return
+  }
+
+  const requestUrl = new URL(event.request.url)
+  const isNavigationRequest = event.request.mode === 'navigate'
+  const isAppShellRequest = requestUrl.origin === self.location.origin && (requestUrl.pathname === '/' || requestUrl.pathname === '/index.html' || requestUrl.pathname === '/manifest.webmanifest')
+
+  if (isNavigationRequest || isAppShellRequest) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, copy)
+          })
+          return response
+        })
+        .catch(() => caches.match(event.request).then((cached) => cached ?? caches.match('/index.html'))),
+    )
     return
   }
 
